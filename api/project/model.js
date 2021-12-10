6170fbca945939a6ed08527b858a19ddd9918a8a
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-10 15:12:30 -0600

    sucesfully retrieving all projects in table from endpoint

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,17 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+async function getProjects() {
+    const projects = await db('projects')
+
+    return projects.map((project) => {
+        return {
+            ...project,
+            project_completed: project.project_completed === 1,
+        }
+    })
+}
+
+module.exports = {
+    getProjects,
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,4 +1,14 @@
 // build your `/api/projects` router here
 const router = require('express').Router()
+const Project = require('./model')
+
+router.get('/', (req, res, next) => {
+    Project.getProjects()
+        .then((projects) => {
+            res.json(projects)
+        })
+        .catch(next)
+})
+
 
 module.exports = router
