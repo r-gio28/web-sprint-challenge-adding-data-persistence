c2506e4f2eecf13927c8e117bac7ff4bf35b4224
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-10 12:51:06 -0600

    added columns to project table, resources table, tasks table, and project_resources table. able to see columns in sqlite3

--- data/migrations/20211210175653_initial-migration.js
+++ data/migrations/20211210175653_initial-migration.js
@@ -1,24 +1,53 @@
 
 exports.up = async function(knex) {
   await knex.schema
-  .createTable('',() => {
-
+  .createTable('projects', (table) => {
+    table.increments('project_id')
+    table.string('project_name', 128).notNullable()
+    table.string('project_description', 255)
+    table.boolean('project_completed').defaultTo(false) 
   })
-   .createTable('',() => {
-      
+   .createTable('resources', (table) => {
+        table.increments('resource_id')
+        table.string('resource_name', 128).notNullable().unique()
+        table.string('resource_description', 255)  
   })
-   .createTable('',() => {
-      
+   .createTable('tasks', (table) => {
+        table.increments('task_id')
+        table.string('task_description', 255).notNullable()
+        table.string('task_notes', 255)
+        table.boolean('task_completed').defaultTo(false)
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
   })
-     .createTable('',() => {
-      
+     .createTable('project_resources', (table) => {
+        table.increments('project_resource_id')
+        table.integer('project_id')
+            .notNullable()
+            .unsigned()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        table.integer('resource_id')
+            .notNullable()
+            .unsigned()
+            .references('resource_id')
+            .inTable('resources')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
   })
 };
 
 exports.down = async function(knex) {
     await knex.schema
-    .dropTableIfExists('')
-    .dropTableIfExists('')
-    .dropTableIfExists('')
-    .dropTableIfExists('')
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
 };
